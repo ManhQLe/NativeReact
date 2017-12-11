19e1b11decc3af2ed106f1edf58337e4a9e14e92
Manh Le <quang.manh@gmail.com>
2017-12-10 21:27:50 -0600

    First blood

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+
+	componentDidMount(){
+		console.log("Debug Line1")
+		debugger
+		console.log("Debug Line2")
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text>How cool is this</Text>
+				<Text>From Manh Le</Text>
+				<Text>Open up App.js to start working on your app!</Text>
+				<Text>Changes you make will automatically reload.</Text>
+				<Text>Shake your phone to open the developer menu.</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
