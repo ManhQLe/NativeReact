06ff3da1ecb1546e1553f66eeff4feac8ac24a95
Manh Le <quang.manh@gmail.com>
2017-12-19 20:39:32 -0600

    Stepper and slider placeholder

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,12 +1,66 @@
 import React,{Component} from 'react'
 import {View,Text} from 'react-native'
 import {getMetricMetaInfo} from '../utils/helpers'
+import UdacitySlider from './UdacitySlider'
+import UdacityStepper from './UdacityStepper'
 
 export default class AddEntry extends Component{
+    state={
+        run:0,
+        bike:0,
+        swim:0,
+        sleep:0,
+        eat:0
+    }
+
+    increment= (metric)=>{
+        const {max,step} = getMetricMetaInfo(metric);
+        this.setState(state=>{
+            const count = state[metric] + step;
+            return {                
+                [metric]:Math.max(count,max)
+            }            
+        })
+    }
+    decrement= (metric)=>{
+        const {step} = getMetricMetaInfo(metric);
+        this.setState(state=>{
+            const count = state[metric] - step;
+            return {                
+                [metric]:Math.max(count,0)
+            }            
+        })
+    }
+
+    slice = (metric,value) =>{
+        this.setState({
+            [metric]: value
+        })
+    }
+
     render(){
+        const metaInfo = getMetricMetaInfo();
         return (
             <View>
-                <Text>Add Entry</Text>
+                {
+                    Object.keys(metaInfo).map(key=>{
+                        const {getIcon,type,...rest} = getMetricMetaInfo(key);
+                        const value = this.state[key]
+                        return <View key={key}>
+                            {getIcon()}
+                            {
+                                type==='slider'
+                                ?<UdacitySlider value={value} onchange={(v)=>this.slide(key,value)}/>
+                                :<UdacityStepper value={value} 
+                                    onIncrement={v=>this.increment(key)}
+                                    onDecrement={v=>this.decrement(key)}
+                                />                                
+                            } 
+                        </View>
+                        
+                    })
+
+                }
             </View>
         )
     }
---
+++ components/UdacityStepper.js
@@ -0,0 +1,9 @@
+import React from'react'
+
+import {View,Text} from 'react-native'
+
+export default function UdacitySlider () {
+    return <View>
+        <Text>Stepper</Text>
+    </View>
+}
