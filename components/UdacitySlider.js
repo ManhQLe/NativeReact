b34d370e7ca697e493d528392256044ad9e2da1f
Manh Le <quang.manh@gmail.com>
2018-01-03 23:07:18 -0600

    Styled Reset button, Sliders

--- components/UdacitySlider.js
+++ components/UdacitySlider.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { StyleSheet, View, Text, Slider } from 'react-native'
+import { gray } from '../utils/colors';
 
 export default function UdacitySlider({ max, unit, step, value, onChange }) {
 
-    return <View style={styles.container}>
-        <Slider
+    return <View style={styles.row}>
+        <Slider style={{flex:1}}
             step={step}
             value={value}
             maximumValue={max}
             minimumValue={0}
             onValueChange={onChange} />
-        <View>
-            <Text>{value}</Text>
-            <Text>{unit}</Text>
+        <View style={styles.metricCounter}>
+            <Text style={{fontSize:24,textAlign:'center'}}>{value}</Text>
+            <Text style={{fontSize:18,textAlign:'center',color:gray}}>{unit}</Text>
         </View>
     </View>
 }
 
 const styles = StyleSheet.create({
-    container: {
-        
+    row: {
+        flexDirection:'row',
+        alignItems:'center',
+        flex:1
+    },
+    metricCounter:{
+        width:85,
+        justifyContent:'center',
+        alignItems:'center'
     }
 })
